01b3cc05b4326e4199b78e744946c58924637c2c
Froschi1860 <45125983+Froschi1860@users.noreply.github.com>
2022-01-26 14:14:51 +0100

    Update Beer.java
    Refining the findMassOfRawAlcohol() method

--- hammeredTest/src/se/hkr/Beer.java
+++ hammeredTest/src/se/hkr/Beer.java
@@ -3,5 +3,7 @@ package se.hkr;
 public class Beer extends Drink{
     Beer(double volume, double percent) { super(volume, percent); }
 
-    public void findMassOfRawAlcohol() {};
+    public double findMassOfRawAlcohol(Beer beer) {
+       
+    }
 }
