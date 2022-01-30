27a8e3c5e385c91b308a2f85c9f5c3fe78f72b9b
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-01-30 21:25:30 +0100

    Update Beer.java

--- hammeredTest/src/se/hkr/Beer.java
+++ hammeredTest/src/se/hkr/Beer.java
@@ -1,6 +1,6 @@
 package se.hkr;
 
-public class Beer extends Drink implements MassOfRawAlcohol {
+public class Beer extends Drink implements MassOfRawAlcohol <Beer> {
     Beer(double volume, double percent) { super(volume, percent); }
 
     public double findMassOfRawAlcohol(Beer beer) {
