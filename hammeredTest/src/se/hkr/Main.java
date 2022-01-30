8812ee79e8e10111d29180a56143e66b734ac90e
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-01-30 21:32:58 +0100

    Update Main.java

--- hammeredTest/src/se/hkr/Main.java
+++ hammeredTest/src/se/hkr/Main.java
@@ -8,6 +8,10 @@ import java.time.temporal.ChronoUnit;
 public class Main {
 
     public static void main(String[] args) {
+        
+        /*
+        
+        drinks.forEach(System.out::println);   - print out each item in the list of drinks the user submitted */
 
     }
 }
