516904d0090e6c251c27590c74bb13609bf154f6
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-01-30 21:18:01 +0100

    Update Liquor.java

--- hammeredTest/src/se/hkr/Liquor.java
+++ hammeredTest/src/se/hkr/Liquor.java
@@ -1,6 +1,6 @@
 package se.hkr;
 
-public class Liquor extends Drink{
+public class Liquor extends Drink implements MassOfRawAlcohol{
     Liquor(double volume, double percent) { super(volume, percent); }
 
     public void findMassOfRawAlcohol() {};
