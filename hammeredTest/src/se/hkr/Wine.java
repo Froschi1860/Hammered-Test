b9af1f2d9d0b4e230ffc35e24630818d77ded70c
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-01-30 21:18:49 +0100

    Update Wine.java

--- hammeredTest/src/se/hkr/Wine.java
+++ hammeredTest/src/se/hkr/Wine.java
@@ -1,6 +1,6 @@
 package se.hkr;
 
-public class Wine extends Drink{
+public class Wine extends Drink implements MassOfRawAlcohol{
     Wine(double volume, double percent) { super(volume, percent); }
 
     public void findMassOfRawAlcohol() {};
